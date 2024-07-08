735c76571df20b71181c720c1243585665d93de2
amrutha104 <amruthavarshini.b@iamneo.ai>
2024-07-08 12:38:14 +0530

    Update api.js

--- reactapp/src/api.js
+++ reactapp/src/api.js
@@ -1,3 +1,3 @@
-const baseURL = 'https://8080-edbbcdbcdfbeebb314038161aefcbabfbebcfive.premiumproject.examly.io';
+const baseURL = 'https://y51n9pr2f0.execute-api.us-east-1.amazonaws.com/backend';
 
 export default baseURL;
